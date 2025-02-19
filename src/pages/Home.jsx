1ea0a82bbcbd6a7b460cfbf54743ce6b3254cb4c
SangamPradhan <142468434+SangamPradhan@users.noreply.github.com>
2025-02-19 11:41:17 +0545

    added typed.js for typed text animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,36 @@
+import { motion } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js"; // Ensure correct import
 import Layout from "../Layout/Layout";
 
 const Home = () => {
+  // Create a ref for the element where Typed.js will be applied
+  const typedRef = useRef(null);
+
+  useEffect(() => {
+    // Initialize Typed.js
+    const options = {
+      strings: ["Sangam Pradhan", "Software Engineer", "React Developer"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true,
+    };
+
+    const typed = new Typed(typedRef.current, options);
+
+    // Clean up the Typed instance on component unmount
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <Layout>
-      <h1>Sangam Pradhan</h1>
+      <motion.div>
+        <h1>
+          I am <span ref={typedRef}/> 
+        </h1>
+      </motion.div>
     </Layout>
   );
 };
